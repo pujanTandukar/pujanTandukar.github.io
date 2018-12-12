d52b64097b2a607f78b58678aede00c04217e78d
Pujan Tandukar <pujan@rgnt2-125-15-dhcp.int.colorado.edu>
2018-12-12 09:16:56 -0700

    portfolio

---
+++ precache-manifest.633b70cdca361c061a23e4789e7ac783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2082fe1b4a7d561d683f",
+    "url": "www.pujantandukar.com/static/js/runtime~main.2082fe1b.js"
+  },
+  {
+    "revision": "e04dd3867d70e076b119",
+    "url": "www.pujantandukar.com/static/js/main.e04dd386.chunk.js"
+  },
+  {
+    "revision": "6a8c248e1c0947f14ea5",
+    "url": "www.pujantandukar.com/static/js/1.6a8c248e.chunk.js"
+  },
+  {
+    "revision": "e04dd3867d70e076b119",
+    "url": "www.pujantandukar.com/static/css/main.f6d5ba79.chunk.css"
+  },
+  {
+    "revision": "d5e3d2aaade30f24ebc9ff78993b31f1",
+    "url": "www.pujantandukar.com/index.html"
+  }
+];
